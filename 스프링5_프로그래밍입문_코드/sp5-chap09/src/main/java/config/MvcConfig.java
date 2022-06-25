7f57b368aabd4e11aad0741b79aaa3694abe4f7e
wonjaechoi97 <wj5295@naver.com>
2022-06-25 23:34:26 +0900

    chap-09

--- 스프링5_프로그래밍입문_코드/sp5-chap09/src/main/java/config/MvcConfig.java
+++ 스프링5_프로그래밍입문_코드/sp5-chap09/src/main/java/config/MvcConfig.java
@@ -7,18 +7,20 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc//mvc를 사용하는데 필요한 다양한 설정을 생성
-public class MvcConfig implements WebMvcConfigurer{
+@EnableWebMvc//1.mvc를 사용하는데 필요한 다양한 설정을 생성
+public class MvcConfig implements WebMvcConfigurer{ //WebMvcConfigurer 은 webmvc내부 설정 조정할 때 사용 
 	
 	
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) { //매핑 경로를 '/'로 주었을 때, jsp/html/css 등을 올바르게 처리하기 위한 설정 추가
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) { //2.dispatcherservlet의 매핑 경로를 '/'로 주었을 때, jsp/html/css 등을 올바르게 처리하기 위한 설정 추가
 		// TODO Auto-generated method stub
 		configurer.enable();
 	}
 	
 	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
+	public void configureViewResolvers(ViewResolverRegistry registry) { //3.jsp이용해서 controller 실행 결과를 보여주기 위한 설정 
 		registry.jsp("/WEB-INF/view/",".jsp");
 	}
+	
+	//1,2,3 webmvc를 위한 
 }
