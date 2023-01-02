9e6b542398367b031efaa735e095eb20b77d2793
wj529 <wj5295@naver.com>
2023-01-03 00:33:13 +0900

    20230102 스프링 핵심 원리 이해1 - 회원 도메인 실행과 테스트

--- SpringLecture/Review/chap01/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ SpringLecture/Review/chap01/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); //구현부까지 의존해서 문제다.
     //다형성에 의해서 MemberRepository타입 변수가 MemoryMemberRepository객체를 참조할 수 있다.
 
     @Override
