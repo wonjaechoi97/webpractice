3eddb89db3a958e1b62c39d0b566f5985b58dd01
wj529 <wj5295@naver.com>
2023-01-08 23:06:48 +0900

    20230108 스프링 핵심 원리 이해2 - 새로운 할인 정책 적용과 문제점 복습

--- SpringLecture/Review/chap02/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringLecture/Review/chap02/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,13 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // FixDiscountPolicy -> RateDiscountPolicy
+    /*
+    OrderServiceImpl이 구체 클래스에도 의존하고 있다.DIP 위반
+    할인 정책을 바꾸는 순간 클라이언트 코드가 **변경**되므로 OCP위반
+
+     */
+    private DiscountPolicy discountPolicy; // 인터페이스에만 의존한다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
