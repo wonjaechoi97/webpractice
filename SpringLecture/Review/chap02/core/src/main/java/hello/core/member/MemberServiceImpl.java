ce276f41270b1fc49dfc01eae43cdf0eb15d3721
wj529 <wj5295@naver.com>
2023-01-09 01:12:53 +0900

    20230108 스프링 핵심 원리 이해2 - 관심사의 분리 복습

--- SpringLecture/Review/chap02/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ SpringLecture/Review/chap02/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,14 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); //구현부까지 의존해서 문제다.
+    private final MemberRepository memberRepository; //구현부까지 의존해서 문제다. ->이제 추상화에만 의존
     //다형성에 의해서 MemberRepository 타입 변수가 MemoryMemberRepository 객체를 참조할 수 있다.
+
+
+    //생성자 주입
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- SpringLecture/Review/chap02/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringLecture/Review/chap02/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,14 +9,20 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //private final MemberRepository memberRepository= new MemoryMemberRepository();
     //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // FixDiscountPolicy -> RateDiscountPolicy
     /*
     OrderServiceImpl이 구체 클래스에도 의존하고 있다.DIP 위반
     할인 정책을 바꾸는 순간 클라이언트 코드가 **변경**되므로 OCP위반
 
      */
+    private final MemberRepository memberRepository;
     private DiscountPolicy discountPolicy; // 인터페이스에만 의존한다.
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
