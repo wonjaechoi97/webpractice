43e3a203cea6edbd13d18c6c2da9c0f256b302d3
wj529 <wj5295@naver.com>
2023-01-11 03:30:59 +0900

    20230108 스프링 핵심 원리 이해2 - AppConfig 리팩토링 복습

--- SpringLecture/Review/chap02/core/src/main/java/hello/core/AppConfig.java
+++ SpringLecture/Review/chap02/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +12,25 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig { //Application 전체를 설정하고 구성한다는 의미
 
+    //역할이 드러나도록 수정
+    /*
+    메서드 명을 통해 역할이 드러남
+     */
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository(); //<--db로 바꾸더라도 이부분만 바꾸면 되므로 간편함
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+    }
+
 
 }
