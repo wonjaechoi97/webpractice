440ee75818dabd978a0711245d33f799f714148a
wj529 <wj5295@naver.com>
2023-01-13 01:31:15 +0900

    20230113 스프링 핵심 원리 이해2 - 스프링으로 전환하기 복습

--- SpringLecture/Review/chap02/core/src/main/java/hello/core/AppConfig.java
+++ SpringLecture/Review/chap02/core/src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { //Application 전체를 설정하고 구성한다는 의미
-
+    //사용 영역의 코드의 변경 없이
+    // 구서 영역의 코드만 변경하여 확장가능하다.
     //역할이 드러나도록 수정
     /*
     메서드 명을 통해 역할이 드러남
      */
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemberRepository memberRepository() {
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemoryMemberRepository(); //<--db로 바꾸더라도 이부분만 바꾸면 되므로 간편함
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new RateDiscountPolicy();
+        return new RateDiscountPolicy(); //<---여기만 고치면 할인 정책 변경가능
     }
 
 
