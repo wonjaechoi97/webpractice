b525aec4c93c0772d0f9a38c9943024a0510ecaf
wj529 <wj5295@naver.com>
2023-01-15 00:04:44 +0900

    chap03 기본 세팅

---
+++ SpringLecture/Review/chap03/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository; //구현부까지 의존해서 문제다. ->이제 추상화에만 의존
+    //다형성에 의해서 MemberRepository 타입 변수가 MemoryMemberRepository 객체를 참조할 수 있다.
+
+
+    //생성자 주입
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
