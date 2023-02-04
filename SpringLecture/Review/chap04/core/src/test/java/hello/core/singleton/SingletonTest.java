6cfe1dd462bafc3692470689dcf26c85cb3fc1c2
wj529 <wj5295@naver.com>
2023-02-04 23:13:13 +0900

    20230204 싱글톤 컨테이너 - 싱글톤 패턴 복습

--- SpringLecture/Review/chap04/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ SpringLecture/Review/chap04/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -27,4 +27,18 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);*/
 
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //same == : 즉 인스턴스 주소가 같느냐
+        // equals : 값이 같느냐
+
+    }
 }
