d24173fd55590a238c7b55280d1193fdea23e25f
hyundai-wonjae <wj5295@naver.com>
2023-02-07 21:06:58 +0900

    20230207 의존관계 자동 주입 -  롬복과 최신 트렌드 : 생성자 하나 밖에 없으면 @Autowired 생략 가능

--- SpringLecture/Review/chap05/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringLecture/Review/chap05/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,10 +6,12 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final 붙은 멤버 주입하는 생성자 만들어줌
 public class OrderServiceImpl implements OrderService{
 
     //private final MemberRepository memberRepository= new MemoryMemberRepository();
@@ -19,8 +21,8 @@ public class OrderServiceImpl implements OrderService{
     할인 정책을 바꾸는 순간 클라이언트 코드가 **변경**되므로 OCP위반
 
      */
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy; // 인터페이스에만 의존한다.
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy; // 인터페이스에만 의존한다.
 
     //setter 의존 관계 주입은 빈 생성 후 의존관계 주입 단계에서 주입된다.
     /*@Autowired
@@ -37,12 +39,12 @@ public class OrderServiceImpl implements OrderService{
 
 
     //스프링이 빈 생성 시 의존관계 주입이 필요하면 그때 같이 주입한다.
-    @Autowired //생성자가 딱 1개 있을 때는 !!생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    //@Autowired //생성자가 딱 1개 있을 때는 !!생략 가능
+    /*public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("1. OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/ // <--------롬복으로 대체
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
