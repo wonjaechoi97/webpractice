0697d5b68af31bd7e2dc059363fe6b0b8db1ba19
hyundai-wonjae <wj5295@naver.com>
2023-02-09 20:15:11 +0900

    20230209 빈 생명주기 콜백- 세팅

---
+++ SpringLecture/Review/chap05~06/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,46 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //테스틍 위해 구체 타입으로 꺼내오기
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        
+        //세개 모두 같다. 싱글톤!!
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class); //AppConfig도 빈으로 생성됨
+
+        System.out.println("bean = " + bean.getClass()); //클래스 타입
+
+    }
+}
---
+++ SpringLecture/Review/chap07/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,63 @@
+package hello.core.order;
+
+import hello.core.annotation.MainDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+//@RequiredArgsConstructor // final 붙은 멤버 주입하는 생성자 만들어줌
+public class OrderServiceImpl implements OrderService{
+
+    //private final MemberRepository memberRepository= new MemoryMemberRepository();
+    //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // FixDiscountPolicy -> RateDiscountPolicy
+    /*
+    OrderServiceImpl이 구체 클래스에도 의존하고 있다.DIP 위반
+    할인 정책을 바꾸는 순간 클라이언트 코드가 **변경**되므로 OCP위반
+
+     */
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy; // 인터페이스에만 의존한다.
+
+    //setter 의존 관계 주입은 빈 생성 후 의존관계 주입 단계에서 주입된다.
+    /*@Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }*/
+
+
+    //스프링이 빈 생성 시 의존관계 주입이 필요하면 그때 같이 주입한다.
+    //@Autowired //생성자가 딱 1개 있을 때는 !!생략 가능
+    public OrderServiceImpl(MemberRepository memberRepository,  @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    } // <--------롬복으로 대체
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        //단일 책임 원칙이 잘 지켜져서 할인 정책이 변경되어도 Order 서비스는 건들 필요가 없다.
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+}
