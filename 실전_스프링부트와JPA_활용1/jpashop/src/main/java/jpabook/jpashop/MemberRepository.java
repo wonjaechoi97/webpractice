1a6bece99355ad8b9b059d67d0a78966c02c4a49
wj529 <wj5295@naver.com>
2023-04-01 00:46:33 +0900

    20230331 - JPA와 DB 설정, 동작확인

---
+++ 실전_스프링부트와JPA_활용1/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext //엔티티 매니저 스프링 컨테이너가 자동으로 주입
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId(); // 커맨드와 쿼리랑 분리하라 커맨드성 없애고 아이디 정도만
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ 실전_스프링부트와JPA_활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional // test에 있으면 트랜잭션 진행 후 롤백
+    @Rollback(value = false)
+    public void testMember() {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member" + (findMember == member)); //같은 영속성 컨텍스트에서 아이디가
+        //같으면 같은 객체
+
+    }
+}
