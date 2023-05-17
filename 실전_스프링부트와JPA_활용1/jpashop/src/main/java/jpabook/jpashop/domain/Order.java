2b9b59187f002e2fd50f5fc529dd77419fc4c167
wj529 <wj5295@naver.com>
2023-05-18 00:37:39 +0900

    20230518 - 주문 리포지토리 개발

--- 실전_스프링부트와JPA_활용1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 실전_스프링부트와JPA_활용1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -64,5 +64,51 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /*
+    주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel(); //여러 개 주문 하나씩 각각 order cancel 날려줘야함
+        }
+    }
+
+    //==조회 로직==//
+     /*
+     전체 상품 전체 가격 조회
+      */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+/*
+        return orderItems.stream().
+                mapToInt(OrderItem::getTotalPrice).
+                sum();
+                */
+    }
+
+
 
 }
--- 실전_스프링부트와JPA_활용1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ 실전_스프링부트와JPA_활용1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,11 +1,13 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
