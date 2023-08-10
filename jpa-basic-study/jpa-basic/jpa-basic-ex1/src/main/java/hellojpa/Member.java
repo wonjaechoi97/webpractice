d0db1e87947c6928f00796339c0c974d4d0058c8
wj529 <wj5295@naver.com>
2023-08-10 22:45:38 +0900

    엔티티 매핑 : 데이터 베이스 스키마 자동 생성, ddl 생성 기능을 통한 제약 조건 추가 학습

--- jpa-basic-study/jpa-basic/jpa-basic-ex1/src/main/java/hellojpa/Member.java
+++ jpa-basic-study/jpa-basic/jpa-basic-ex1/src/main/java/hellojpa/Member.java
@@ -1,15 +1,18 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity //jpa 가 로딩시 jpa 사용하는 것을 인식
-//@Table(name = "USER")
+//@Table(name = "USER") <-- 테이블 명으로 매핑
 public class Member {
     @Id
     private  Long id;
+    @Column(unique = true, length = 10) //unique 제약 조건, 길이 10자 제한 조건 추가
     private String name;
+    private int age;
 
     public Member() {
 
